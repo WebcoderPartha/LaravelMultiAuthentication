4f0d253de4fd9422fb4177023e381624e05f1cbf
Parthadeb Mondal <you@example.com>
2020-08-17 02:40:42 +0600

    Middleware conditions done

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['as' => 'admin', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function (){
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']], function (){
     Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 });
 
-Route::group(['as' => 'author', 'prefix' => 'author', 'namespace' => 'Author', 'middleware'=> 'auth'], function (){
+Route::group(['as' => 'author.', 'prefix' => 'author', 'namespace' => 'Author', 'middleware'=> ['auth', 'author']], function (){
     Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 });
