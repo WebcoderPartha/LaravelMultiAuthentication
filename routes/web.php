5b16a4d2e74e12b60271525e3460748badf767ab
Parthadeb Mondal <you@example.com>
2020-08-17 02:31:26 +0600

    Contoller creation and routing

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as' => 'admin', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function (){
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+});
+
+Route::group(['as' => 'author', 'prefix' => 'author', 'namespace' => 'Author', 'middleware'=> 'auth'], function (){
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
